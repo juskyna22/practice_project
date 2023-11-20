ac08fa52cf0bd3b7ab931a84d9bc622b86b209bf
juskyna22 <66147907+juskyna22@users.noreply.github.com>
2023-11-20 11:55:32 -0700

    Update server.py

--- server.py
+++ server.py
@@ -4,11 +4,24 @@
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
 #Initiate the flask app : TODO
 
+app = Flask("Sentiment Analyzer")
+
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = sentiment_analyzer(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    if label is None:
+        return "Invalid input ! Try again."
+    else:
+        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+
     ''' This code receives the text from the HTML interface and 
         runs sentiment analysis over it using sentiment_analysis()
         function. The output returned shows the label and its confidence 
@@ -18,11 +31,15 @@ def sent_analyzer():
 
 @app.route("/")
 def render_index_page():
+    return render_template('index.html')
+
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
     #TODO
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''#TODO
